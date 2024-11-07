42cfefd71e35356f6e7395857f12cb209caa1a3d
Valentyn Mostovyi <mostovoy.v@gmail.com>
2024-11-07 22:29:57 +0100

    Finish homework

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,12 @@
 import s from "./Contact.module.css"
+import { FaUserLarge, FaPhone } from "react-icons/fa6";
 
 const Contact = ({ data: { id, name, number }, onDelete }) => {
 	return (
 		<div className={s.contact}>
-			{/* TODO add icon */}
 			<ul className={s.list}>
-				<li className={s.field}>{name}</li>
-				<li className={s.field}>{number}</li>
+				<li className={s.field}><FaUserLarge className={s.icon} />{name}</li>
+				<li className={s.field}><FaPhone className={s.icon} />{number}</li>
 			</ul>
 			<button className={s.btn} onClick={() => onDelete(id)}>
 				Delete
